1291fa007b1ceede6263bc728ed5b5fdb77beb22
RamonaEJSpence <ramonaejspence@gmail.com>
2020-09-12 20:57:17 -0400

    added model and controller, added new, index, create

--- app/controllers/ice_creams_controller.rb
+++ app/controllers/ice_creams_controller.rb
@@ -1,5 +1,32 @@
 class IceCreamsController < ApplicationController
+
+    def index
+        @ice_cream = IceCream.all
+    end
+
     def new
     end
 
+    def show
+        @ice_cream = IceCream.find(params[:id])
+    end
+
+    def create
+        @ice_cream = IceCream.new(ice_cream_params)
+
+        @ice_cream.save
+        redirect_to @ice_cream
+    end
+
+    def destroy
+        @ice_cream = IceCream.find(params[:id])
+        @ice_cream.destroy
+
+        redirect_to ice_creams_path
+    end
+
+    private
+        def ice_cream_params
+            params.permit(:name, :description, :available, :featured, :date_churned)
+        end
 end
--- app/models/ice_cream.rb
+++ app/models/ice_cream.rb
@@ -1,2 +1,2 @@
-class IceCream < ApplicationRecord
+class IceCream < ApplicationRecord 
 end
