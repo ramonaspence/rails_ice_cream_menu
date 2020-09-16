e9f530537c312dbf9d2f80dd94b1c20e21010bc9
RamonaEJSpence <ramonaejspence@gmail.com>
2020-09-16 18:10:53 -0400

    adding RSpec for tests

--- app/models/ice_cream.rb
+++ app/models/ice_cream.rb
@@ -3,5 +3,6 @@ class IceCream < ApplicationRecord
     length: { minimum: 3 } 
     validates :description, presence: true,
     length: { minimum: 12 }
-    validates :date_churned, presence: true
+    validates :date_churned, presence: true,
+    unless: Proc.new { |i| i.date_churned > DateTime.now }
 end
---
+++ spec/ice_creams_controller_spec.rb
@@ -0,0 +1,38 @@
+# "what does a test do?
+# A test sets up a context, situation, or environment
+# that would/will/can be in the application itself 
+# (ie: I log in and my credentials are invalid). 
+
+# It defines what the expected result is and compares the two.
+# "
+require 'spec_helper'
+
+
+RSpec.describe IceCreamsController, :type => :controller do
+    
+    context "GET new" do
+        
+
+        it 'assigns a blank ice_cream to the view' do
+            get :new
+            expect(assigns(:ice_cream)).to be_a_new(ice_cream)
+        end
+
+
+    end
+
+    context "POST create" do
+
+
+        it 'redirects to show view for the ice_cream just created' do
+            params = { 
+                ice_cream: { 
+                    name: "chocolate", description: "This is a chocolate ice cream", 
+                    date_churned: DateTime.now, 
+                } 
+            }
+            post :create, params
+            expect(response).to redirect_to(@ice_cream)
+        end
+    end
+end
